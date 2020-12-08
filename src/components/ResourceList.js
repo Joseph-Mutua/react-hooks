65c53d3d9141a29f204c0a95e9a0e2c9e45f7209
Joseph Mutua <Mutuaj793@gmail.com>
2020-12-08 11:54:27 +0300

    Fetching a given resource

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,9 +1,19 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class ResourceList extends Component {
-  state = {};
+  state = { resources: [] };
+
+  async componentDidMount() {
+    const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/${this.props.resource}`
+    );
+
+    this.setState({ resources: response.data });
+  }
+
   render() {
-    return <div>{this.props.resource}</div>;
+    return <div>{this.state.resources.length}</div>;
   }
 }
 
